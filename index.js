525286069684ceca5618b302c13a2ea25435a42a
Barkatzx <barkatullah.zx@gmail.com>
2023-05-12 13:58:45 +0600

    Updated

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000
+const cors = require("cors");
+const port = process.env.Port || 5000;
 
-const chefRecipe = require('./data/chefRecipe.json')
+const chefInfo = require("./data/chefInfo.json");
+const recipes = require("./data/recipeDetails.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Chef Recipe hunter is running')
-})
+app.get("/", (req, res) => {
+  res.send(chefInfo);
+});
 
-app.get('/recipes', (req, res) => {
-  res.json(chefRecipe);
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = chefInfo.find((chef) => chef.chefId === id);
+  res.send(chef);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Chef Website is Running on Port: ${port}`);
+});
